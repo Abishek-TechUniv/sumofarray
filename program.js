09d54c0b2bfa4db61650982d823ee808c1ff1abf
Abishek Aditya <abishekaditya@outlook.com>
2018-01-12 14:20:38 +0530

    replaced false with null

--- program.js
+++ program.js
@@ -3,14 +3,14 @@ function sum(firstArray, secondArray) {
   
   let check = verifyLength(firstArray, secondArray);
   
-  if (!check) return false;
+  if (!check) return null;
   
   for (let i = 0; i < firstArray.length; i++) {
     result = addNumbers(firstArray[i],secondArray[i]);
     if (result)
       resultArray[i] = result;
     else
-      return false;
+      return null;
   }
   return resultArray;
 }
@@ -20,29 +20,28 @@ function addNumbers(first, second) {
   if (check) {
     return first + second;
   } else {
-    return false;
+    return null;
   }
 }
 
 function verifyNumber(number){
   if (typeof number != "number") {
     console.log("type is not number");
-    return false;
-  }
-  else if (number > -Infinity && number < Infinity) {
-    return true;
+    return null;
+  } else if (number > -Infinity && number < Infinity) {
+      return true;
   } else {
-    console.log("not a number error")
-    return false;
+      console.log("NaN or Infinity")
+      return null;
   }
 }
 
 function verifyLength(firstArray, secondArray) {
   if(firstArray.length == secondArray.length) {
     return true;
-  }else {
+  } else {
     console.log("array length mismatch")
-    return false;
+    return null;
   }
 }
 
@@ -50,7 +49,7 @@ function equal(computed, expected) {
   for (let i = 0; i < computed.length; i++) {
     if (computed[i] == expected[i]) {
     } else {
-      return false;
+      return null;
     }
   }
   return true;
@@ -60,26 +59,26 @@ let sum1 = sum([1,2,3,4,5], [6,7,8,9,10])
 console.log("adding two arrays :",equal(sum1,[7,9,11,13,15]))
 
 let sum2 = sum([1,2], [1,4,5])
-console.log("adding two arrays with different lengths :", sum2 === false)
+console.log("adding two arrays with different lengths :", sum2 === null)
 
 let sum3 = sum(["1", 2], [2, 3])
-console.log("adding an array with a string inside it :", sum3 === false)
+console.log("adding an array with a string inside it :", sum3 === null)
 
 let add1 = addNumbers(1, 2)
 console.log("adding two numbers :", add1 === 3)
 
 let add2 = addNumbers(NaN, 2)
-console.log("adding a number with NaN :" , add2 === false)
+console.log("adding a number with NaN :" , add2 === null)
 
 let value1 = verifyNumber(4)
 console.log("verifies 4 :", value1 === true)
 
 let value2 = verifyNumber(Infinity)
-console.log("verifies Infinity as false :", value2 === false)
+console.log("verifies Infinity as false :", value2 === null)
 
 let length1 = verifyLength([1,2,3],[2,3,4])
 console.log("verifies length of array :", length1 === true)
 
 let length2 = verifyLength([1,2,3],[2,3,4,5])
-console.log("verifies length of mismatched array :", length2 === false)
+console.log("verifies length of mismatched array :", length2 === null)
 
