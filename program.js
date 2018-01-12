ced60c4c937c338c5b447d32a933a201e325bf04
Abishek Aditya <abishekaditya@outlook.com>
2018-01-12 13:17:48 +0530

    verifies non numbers

--- program.js
+++ program.js
@@ -1,9 +1,9 @@
 function sum(firstArray, secondArray) {
-  var resultArray = []
-  var check = verifyLength(firstArray, secondArray);
+  let resultArray = []
+  let check = verifyLength(firstArray, secondArray);
   if (check.success == true){
     for (let i = 0; i < firstArray.length; i++) {
-      resultArray[i] = firstArray[i] + secondArray[i];
+      resultArray[i] = add(firstArray[i],secondArray[i]);
     }
     return resultArray;
   } else {
@@ -11,6 +11,28 @@ function sum(firstArray, secondArray) {
     return false;
   }
 
+}
+
+function add(first, second){
+  let check = verify(first) && verify(second);
+  if (check) {
+    return first + second;
+  }
+  else{
+    return false;
+  }
+}
+
+function verify(number){
+  if (typeof number != "number"){
+    console.log("not a number");
+    return false;
+  }
+  if (number > -Infinity && number < Infinity) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function verifyLength(firstArray, secondArray) {
@@ -31,11 +53,23 @@ function equal(computed, expected){
   return true;
 }
 
-var sum1 = sum([1,2,3,4,5], [6,7,8,9,10])
+let sum1 = sum([1,2,3,4,5], [6,7,8,9,10])
 console.log("adding two arrays :",equal(sum1,[7,9,11,13,15]))
 
-var sum2 = sum([1,2], [1,4,5])
+let sum2 = sum([1,2], [1,4,5])
 console.log("adding two arrays with different lengths :", sum2 === false)
 
-var sum3 = sum(["1", 2], [2, 3])
+let sum3 = sum(["1", 2], [2, 3])
 console.log("adding an array with a string inside it :", sum3 === false)
+
+let add1 = add(1, 2)
+console.log("adding two numbers :", add1 == 3)
+
+let add2 = add(NaN, 2)
+console.log("adding a number with NaN :" , add2 == false)
+
+let value1 = verify(4)
+console.log("verifies 4 :", value1 == true)
+
+let value2 = verify(Infinity)
+console.log("verifies Infinity as false :", value2 == false)
