e2748fdc9e7bcaa82a4b0287065280e73249ec2c
Abishek Aditya <abishekaditya@outlook.com>
2018-01-12 12:46:32 +0530

    fails with two different length arrays

--- program.js
+++ program.js
@@ -19,3 +19,6 @@ function equal(computed, expected){
 
 var sum1 = sum([1,2,3,4,5], [6,7,8,9,10])
 console.log("adding two arrays :",equal(sum1,[7,9,11,13,15]))
+
+var sum2 = sum([1,2], [1,4,5])
+console.log("adding two arrays with different lengths :", equal(sum2, false))
